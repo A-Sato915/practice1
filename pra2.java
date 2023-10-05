2ba3596d21148ea9eb45f7fe8bf8ce0c66ad136a
Ayumu <a.satou.sys23@morijyobi.ac.jp>
2023-10-05 14:36:08 +0900

    コードの変更
    Hello Worldを100回出力するようにしました。

--- pra2.java
+++ pra2.java
@@ -1,5 +1,9 @@
 public class pra2 {
     public static void main(String[] args) {
-    	System.out.println("Hello World");
+    	int i = 1;
+    	while(i <= 100){
+    		System.out.println("Hello World");
+        	i++;
+    	}
     }
 }
